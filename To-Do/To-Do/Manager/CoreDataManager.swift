41ce6736e54918fdaf0bb14c18fd9b9d416cb910
Eser kucuker <eserkucuker@gmail.com>
2022-02-11 20:03:45 +0300

    Create CoreData and ManagerContracts

---
+++ To-Do/To-Do/Manager/CoreDataManager.swift
@@ -0,0 +1,37 @@
+//
+//  CoreDataManager.swift
+//  To-Do
+//
+//  Created by Eser Kucuker on 11.02.2022.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataManager : DataManagerProtocol {
+    
+}
+func updateData(todoItem: Tasks, title: String, detail: String, date: Date?) {
+    <#code#>
+}
+
+func saveData() {
+    <#code#>
+}
+
+func fetchData() -> [Tasks] {
+    <#code#>
+}
+
+func deleteData(todoItem: Tasks) {
+    <#code#>
+}
+
+func sortbyCreatedData() -> [Tasks] {
+    <#code#>
+}
+
+func searchData(with: String) -> [Tasks] {
+    <#code#>
+}
+
