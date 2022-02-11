f7bb7a3d91faae92b0abe30de20c7d872d3e8bde
Eser kucuker <eserkucuker@gmail.com>
2022-02-11 22:54:26 +0300

    Add TaskDetailContracts

--- To-Do/To-Do/Screens/TaskDetail/TaskDetailContracts.swift
+++ To-Do/To-Do/Screens/TaskDetail/TaskDetailContracts.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+// MARK: VIEWMODEL
+protocol TaskDetailViewModelProtocol : AnyObject {
+    var delegate : TaskDetailTableViewModelDelegate? { get set }
+    func didLoad()
+    func addTodo(task:TaskDetailPresentation)
+    func goBack()
+}
+
+
+protocol TaskDetailTableViewModelDelegate : AnyObject {
+    func handleOutput(_ output : ViewModelOutput)
+    func navigate(to route : DetailViewRouter)
+}
+
+
+enum DetailViewRouter {
+    case goBack
+    case saveAndBack
+}
+enum ViewModelOutput {
+    case showToDoDetail(TaskDetailPresentation)
+    case showToDoAdd
+    case showAlert
+    
+}
+// MARK: BUILDER SCREEN TYPE
+
+enum builderChoice {
+    case DetailView
+    case AddView
+}
