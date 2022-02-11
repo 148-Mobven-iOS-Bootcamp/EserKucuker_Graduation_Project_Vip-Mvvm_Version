cf34d9a815cbecc715f4627a774a86a41d5ddc38
Eser kucuker <eserkucuker@gmail.com>
2022-02-11 22:04:46 +0300

    Add TaskListPresenter

--- To-Do/To-Do/Screens/TaskList/TaskListPresenter.swift
+++ To-Do/To-Do/Screens/TaskList/TaskListPresenter.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+class TaskListPresenter: TaskListPresenterProtocol {
+    weak var viewController: TaskListViewProtocol?
+    
+    func handeOutput(_ output: TaskListInteractorOutput) {
+        switch output {
+        case .showData(let tasks),.showUpdateData(let tasks):
+            viewController?.handleOutput(.showToDoList(tasks.map(TaskListPresentation.init)))
+        case .sortData(let sortedData, let flag):
+            viewController?.handleOutput(.showSortedToDo(sortedData.map(TaskListPresentation.init), flag))
+        case .showSearchData(let tasks):
+            viewController?.handleOutput(.showSearchToDo(tasks.map(TaskListPresentation.init)))
+        case .deletedata(let tasks):
+            viewController?.handleOutput(.showDeletedToDo(tasks.map(TaskListPresentation.init)))
+        case .showEmptyData:
+            viewController?.handleOutput(.showEmptyData)
+        }
+    }
+}
